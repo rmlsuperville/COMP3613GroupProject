1fed00dcec6316b344cc147edf9f714f7e693cd2
rmlsuperville <raphee54@hotmail.com>
2019-11-16 12:20:13 -0400

    committed changes

--- deploy.py
+++ deploy.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -10,9 +10,12 @@ app.config['MYSQL_DB'] = "SWy5rY8sM1"
 
 mysql = MySQL(app)
 
-
 @app.route('/')
 def index():
+	return render_template('index.html')
+
+@app.route('/employees')
+def employees():
 	cur1 = mysql.connection.cursor()
 	cur2 = mysql.connection.cursor()
 
@@ -23,10 +26,26 @@ def index():
 		empInfo = cur1.fetchall()
 		empRecords = cur2.fetchall()
 
-		return render_template('index.html', empInfo=empInfo, empRecords=empRecords)
+		return render_template('employees.html', empInfo=empInfo, empRecords=empRecords)
 	else:
 		return "<h1>Failure in retrieving data</h1>"
+
+
+@app.route('/employee_search', methods=['POST'])
+def employee_search():
 	
+	empID = request.form['empId']
+	cur = mysql.connection.cursor()
+
+	result = cur.execute("SELECT * FROM EmployeeRecords WHERE EmployeeID = {};". format(empID))
+	if result > 0:
+		empRecords = cur.fetchall()
+
+		return render_template('employee.html', empRecords=empRecords)
+	else:
+		return "<h1>Failure in retrieving data</h1>"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
